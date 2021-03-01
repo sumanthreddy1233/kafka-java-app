6103bc276c83c33de2150c99b8e920505d80f7c7
sumanthreddy1233 <sumanthreddy.naidu@gmail.com>
2021-03-01 14:22:23 -0600

    restored deleted file

---
+++ src/main/java/edu/nwmissouri/sumanth/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.sumanth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+    }
+}
